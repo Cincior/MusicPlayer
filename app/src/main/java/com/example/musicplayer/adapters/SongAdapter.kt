e4057941875ebc0b47dbe29656dcb0bf60fa15bd
Krzys <krzycho75000@gmail.com>
2024-07-31 12:15:20 +0200

    Added feature which looks for all possible songs from Download folder

--- app/src/main/java/com/example/musicplayer/adapters/SongAdapter.kt
+++ app/src/main/java/com/example/musicplayer/adapters/SongAdapter.kt
@@ -1,5 +1,9 @@
 package com.example.musicplayer.adapters
 
+import android.content.Context
+import android.media.MediaPlayer
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +12,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayer.R
 import com.example.musicplayer.model.Song
 
-class SongAdapter(private val items: List<Song>) : RecyclerView.Adapter<SongAdapter.ItemViewHolder>() {
-
+class SongAdapter(private val items: List<Song>, private val context: Context) : RecyclerView.Adapter<SongAdapter.ItemViewHolder>() {
+    private lateinit var mediaPlayer: MediaPlayer
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val titleTextView: TextView = view.findViewById(R.id.songTitle)
         val durationTextView: TextView = view.findViewById(R.id.songDuration)
@@ -21,8 +25,17 @@ class SongAdapter(private val items: List<Song>) : RecyclerView.Adapter<SongAdap
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.titleTextView.text = items[position].title
-        holder.durationTextView.text = items[position].duration.toString()
+        val item = items[position]
+        holder.titleTextView.text = item.title
+        holder.durationTextView.text = item.duration.toString()
+        holder.itemView.setOnClickListener{
+
+            mediaPlayer = MediaPlayer().apply {
+                setDataSource(item.path)
+                prepare()
+                start()
+            }
+        }
     }
 
     override fun getItemCount() = items.size
--- app/src/main/java/com/example/musicplayer/viewmodel/SongViewModel.kt
+++ app/src/main/java/com/example/musicplayer/viewmodel/SongViewModel.kt
@@ -4,18 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.musicplayer.model.Song
+import com.example.musicplayer.model.SongsFinder
 
 class SongViewModel : ViewModel() {
 
-    private val _items = MutableLiveData<List<Song>>()
-    val items: LiveData<List<Song>> get() = _items
+    private val _items = MutableLiveData<ArrayList<Song>>()
+    val items: LiveData<ArrayList<Song>> get() = _items
 
     init
     {
-        _items.value = listOf(
-            Song(1, "Item 1", 2.40, "empty"),
-            Song(1, "Item 1", 2.40, "empty"),
-            Song(1, "Item 1", 2.40, "empty")
-        )
+        val SF = SongsFinder()
+        val songList = SF.getSongsFromDwonload()
+        _items.value = songList
     }
 }
