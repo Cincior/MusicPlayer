f2f1790323c7a0fdae981b17f1bf4187b9f7882e
Krzys <krzycho75000@gmail.com>
2024-08-02 17:02:47 +0200

    Added some documentation and some small improvements

--- app/src/main/java/com/example/musicplayer/model/SongsFinder.kt
+++ app/src/main/java/com/example/musicplayer/model/SongsFinder.kt
@@ -1,7 +1,6 @@
 package com.example.musicplayer.model
 
 import android.media.MediaMetadataRetriever
-import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -9,21 +8,17 @@ import java.io.File
 
 class SongsFinder()
 {
-
-    fun getSongs(): ArrayList<Song>
-    {
-        val songList = arrayListOf(
-            Song(1, "test", 20000, "/storage/emulated/0/Download/SHINY FINALLLLLL WAV.wav")
-        )
-        return songList
-    }
-
+    /**
+     * Async method - getting all audio files takes a while
+     * Looks for audio files in Download directory
+     * @return list of founded songs in given directory
+     */
     suspend fun getSongsFromDownload(): ArrayList<Song> = withContext(Dispatchers.IO)
     {
         var id = 0
         val songList = ArrayList<Song>()
-        val getFolder = File("/storage/emulated/0/Download") // THIS PATH CAN CHANGE
-        //val getFolder = File("/sdcard/Download/") // THIS PATH CAN CHANGE
+        val folderPath = "/storage/emulated/0/Download/"
+        val getFolder = File(folderPath) // THIS PATH CAN CHANGE
         if (getFolder.exists() && getFolder.isDirectory)
         {
             val files = getFolder.listFiles()
@@ -33,7 +28,7 @@ class SongsFinder()
                 {
                     if (file.isFile && ( file.extension.equals("mp3", ignoreCase = true) || file.extension.equals("wav", ignoreCase = true)))
                     {
-                        songList.add(Song(id, file.name, (getSongDuration("/storage/emulated/0/Download/" + file.name)), "/storage/emulated/0/Download/" + file.name))
+                        songList.add(Song(id, file.name, (getSongDuration(folderPath + file.name)), folderPath + file.name))
                     }
                     id++
                 }
@@ -42,23 +37,32 @@ class SongsFinder()
         return@withContext songList
     }
 
+    /**
+     * Getting song duration with MediaMetadataRetriever
+     * @param filePath path to audio file
+     * @return duration in milliseconds or 0 if file doesn't exist
+     */
     private fun getSongDuration(filePath: String): Long // TIME IN MILLISECONDS
     {
         val file = File(filePath)
-        if (!file.exists()) {
-            println("File does not exist: $filePath")
+        if (!file.exists())
+        {
             return 0L
         }
 
         val retriever = MediaMetadataRetriever()
-        return try {
+        return try
+        {
             retriever.setDataSource(filePath)
             val durationStr = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
             durationStr?.toLongOrNull() ?: 0L
-        } catch (e: IllegalArgumentException) {
-            println("Failed to retrieve duration for file: $filePath")
+        }
+        catch (e: IllegalArgumentException)
+        {
             0L
-        } finally {
+        }
+        finally
+        {
             retriever.release()
         }
     }
